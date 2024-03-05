adfa957349fa4c70e969035e6fe2bc3a7a53512c
Jasmin Causevic <jcausevi@gmail.com>
2024-03-05 18:59:22 +0100

    Added Slidesho.js component

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,3 +1,14 @@
 /** Here you can write the code for your slideshow component
  *  you can use the images placed in the slideshowData file
  */
+
+
+function Slideshow (){
+  return(
+    <>
+      <h1>Slideshow</h1>
+    </>
+  )
+}
+
+export { Slideshow }
